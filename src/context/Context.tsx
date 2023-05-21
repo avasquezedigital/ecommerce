077247814e30bb2d3b1cf0b0b09320f5d13489b1
Andres Vasquez <andreswd001@gmail.com>
2023-05-21 16:54:03 -0500

    Pruebas values

--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -7,10 +7,12 @@ type Props = {
     children: React.ReactNode
 }
 export const ShoppingCartProvider = ({ children }: Props) => {
-    const [products, setProducts] = useState<Product[]>([]);
-
+    const [products, setProducts] = useState([]);
+    
     return (
-        <ShoppingCartContext.Provider value={{}}>
+        <ShoppingCartContext.Provider value={{
+            products: products
+        }}>
             {children}
         </ShoppingCartContext.Provider>
     )
